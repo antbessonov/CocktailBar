1c0ccb2d01db9da9e5f4bb0e486e1e61b92f88fe
Anton Bessonov <anton@bessonov.org>
2023-08-09 03:26:39 +0700

    Добавление ingredientList в Cocktail.kt

--- domain/src/main/java/org/bessonov/cocktailbar/domain/model/Cocktail.kt
+++ domain/src/main/java/org/bessonov/cocktailbar/domain/model/Cocktail.kt
@@ -2,6 +2,7 @@ package org.bessonov.cocktailbar.domain.model
 
 data class Cocktail(
     val title: String,
+    val ingredientList: String,
     val description: String?,
     val recipe: String?
 )
