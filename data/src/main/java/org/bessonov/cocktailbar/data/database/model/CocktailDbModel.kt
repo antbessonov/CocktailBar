8ea732af5f66fcf07e6494278d08adb0e6f7f902
Anton Bessonov <anton@bessonov.org>
2023-08-08 15:55:42 +0700

    Создание CocktailDbModel.kt

---
+++ data/src/main/java/org/bessonov/cocktailbar/data/database/model/CocktailDbModel.kt
@@ -0,0 +1,12 @@
+package org.bessonov.cocktailbar.data.database.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "cocktails")
+data class CocktailDbModel(
+    @PrimaryKey
+    val title: String,
+    val description: String?,
+    val recipe: String?
+)
